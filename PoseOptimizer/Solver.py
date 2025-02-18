f56b3d282ae6c4eeee4a4eed53e786871d76c61f
JunseoMin <minjs4562@gmail.com>
2025-02-18 17:34:06 +0900

    feat: mean solver

--- PoseOptimizer/Solver.py
+++ PoseOptimizer/Solver.py
@@ -1,3 +1,37 @@
-class Solver():
-    def __init__(self):
-        pass
+import numpy as np
+from einops import rearrange
+from util.math import averageSE3
+
+class poseSolver():
+    def __init__(self, type = "meansolver"):
+        assert type in ["meansolver","reproject"], "solver type should be 'meansolver' or 'reproject' "
+        
+        self.type = type
+
+    def mean_solve(self, transformations:np.ndarray):
+        r"""
+        solves transformation matrixes mean
+        ### input: 
+            Object transformations with shape (n_camera, n_objects, SE(3) matrix(4,4)).
+        ### output:
+            Object transformations with shape (n_objects, SE(3) matrix(4,4)).
+        """
+
+        n_camera, n_obj, _, _ = transformations.shape
+        transformations = rearrange(transformations, "c o r col -> o c r col")
+        
+        res = []
+        for i in range(n_obj):
+            avg = averageSE3(transformations[i])
+            res.append(avg)
+
+        return np.array(res)
+
+
+    def solve(self, transformation):
+        if self.type == "meansolver":
+            res = self.mean_solve(transformation)
+            return res
+        if self.type == "reproject":
+            #TODO
+            pass
