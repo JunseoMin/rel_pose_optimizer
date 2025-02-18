46d5a25eb405c30958497bb8d4be30bbc24f8136
JunseoMin <minjs4562@gmail.com>
2025-02-18 15:04:35 +0900

    feat: camera rel pose from overlapping marker

---
+++ PoseOptimizer/CamRelPoseFromMarker.py
@@ -0,0 +1,40 @@
+import numpy as np
+from einops import rearrange
+from util import *
+
+def CamRelPoseFromMarker(transformation:np.ndarray):
+    r"""
+    @brief Calc camera relative transformation from overlapping AR marker
+    @param transformations: np.ndarray type with (number of cameras, number of objects, SE(3))
+
+    ### returns
+        [T_c1c2, T_c1c3, Tc2c3 ...]
+    """
+
+    camera_rels = []
+
+    n_camera    = transformation.shape[0]
+    n_objects   = transformation.shape[1]
+    newT        = rearrange(transformation, "c o row col -> o c row col") # rearrange to (number of objects, number of cameras, SE(3))
+
+    for i in range(n_objects):
+        if len(camera_rels):
+            # If relative position extracted
+            # Add another option if needed
+            break
+
+        if np.any(np.isnan(newT[i])):   # pass if i'th object observation contains NaN
+            continue
+        
+        object_rels = []
+
+        for c1 in range(n_camera):
+            for c2 in range(c1 + 1, n_camera):
+                Tc1c2 = newT[i][c1] @ inverseSE3(newT[i][c2])
+                object_rels.append(Tc1c2)
+        
+        camera_rels.append(object_rels)
+
+    # print(newT) # debug
+
+    return np.array(camera_rels)
